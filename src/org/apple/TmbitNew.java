aeb4f7dada040e00d6833340ba2bc1538e620d30
Bala29598 <balabalajkm7@gamil.com>
2023-06-14 12:00:09 +0530

    secert

---
+++ src/org/apple/TmbitNew.java
@@ -0,0 +1,18 @@
+package org.apple;
+
+public class TmbitNew {
+
+	public void computerModel() {
+
+		System.out.println("The computer model1: Acer");
+	}
+
+	public void computerMode2() {
+
+		System.out.println("The computer model2: Apple");
+	}
+	
+	
+	
+	
+}
